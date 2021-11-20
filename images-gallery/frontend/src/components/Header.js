71c375fcfcd5cf7883156350d5d4a546e8053083
Giovanni Pais <giom.pais@gmail.com>
2021-11-19 22:42:52 -0300

    controlled search component

--- images-gallery/frontend/src/components/Header.js
+++ images-gallery/frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
     backgroundColor: 'lightblue'
@@ -9,7 +9,11 @@ const Header = (props) => {
     const { title } = props;
     return (
         <Navbar style={navbarStyle} variant="light">
+            <Container>
+
             <Navbar.Brand href="/">{title}</Navbar.Brand>
+
+            </Container>
         </Navbar>
     )
 };
